e3ec2e44bed6504577a2af7f1e0d300bb155cb20
Juhwan <dev.juhwanlee@gmail.com>
2022-03-29 23:47:56 +0900

    Add code: validation

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -6,20 +6,38 @@ import Button from '../UI/Button';
 import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState();
-  const [enteredPhone, setEnteredPhone] = useState();
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredPhone, setEnteredPhone] = useState('');
+  const [enteredContent, setEnteredContent] = useState('');
 
   const addUserHandler = (event) => {
     event.preventDefault();
+    if (
+      enteredUsername.trim().length === 0 ||
+      enteredPhone.length === 0 ||
+      enteredContent.trim().length === 0
+    ) {
+      return;
+    }
+    if (+enteredPhone < 0) {
+      return;
+    }
+    console.log(enteredUsername, enteredPhone, enteredContent);
+    setEnteredUsername('');
+    setEnteredPhone('');
+    setEnteredContent('');
   };
 
   const usernameChangeHandler = (event) => {
     setEnteredUsername(event.target.value);
-    console.log(enteredUsername, enteredPhone);
   };
 
   const phoneChangeHandler = (event) => {
     setEnteredPhone(event.target.value);
+  };
+
+  const contentChangeHandler = (event) => {
+    setEnteredContent(event.target.value);
   };
 
   return (
@@ -27,14 +45,30 @@ const AddUser = (props) => {
       <form onSubmit={addUserHandler}>
         <div className={classes.inputFlex}>
           <label htmlFor="username">방문자명</label>
-          <input id="username" type="text" onChange={usernameChangeHandler} />
+          <input
+            id="username"
+            type="text"
+            value={enteredUsername}
+            onChange={usernameChangeHandler}
+          />
           <label htmlFor="phone">연락처</label>
-          <input id="phone" type="number" onChange={phoneChangeHandler} />
+          <input
+            id="phone"
+            type="number"
+            value={enteredPhone}
+            onChange={phoneChangeHandler}
+          />
         </div>
         <label htmlFor="content" className={classes.blind}>
           내용
         </label>
-        <textarea id="content" rows="" cols="" required />
+        <textarea
+          id="content"
+          rows=""
+          cols=""
+          value={enteredContent}
+          onChange={contentChangeHandler}
+        />
         <Button type="submit">등록하기</Button>
       </form>
     </Card>
