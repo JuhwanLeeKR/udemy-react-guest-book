fc0653d6a8c3a0ccee043a4953658f43598451b3
Juhwan <dev.juhwanlee@gmail.com>
2022-03-29 22:55:26 +0900

    Add code: card component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,19 +1,29 @@
 import React from 'react';
 
+import Card from '../UI/Card';
+
+import classes from './AddUser.module.css';
+
 const AddUser = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
   };
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">방문자명</label>
-      <input id="username" type="text" />
-      <label htmlFor="phone">연락처</label>
-      <input id="phone" type="number" />
-      <label htmlFor="content">내용</label>
-      <textarea id="content" rows="" cols="" required />
-      <button type="submit">등록하기</button>
-    </form>
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <div className={classes.inputFlex}>
+          <label htmlFor="username">방문자명</label>
+          <input id="username" type="text" />
+          <label htmlFor="phone">연락처</label>
+          <input id="phone" type="number" />
+        </div>
+        <label htmlFor="content" className={classes.blind}>
+          내용
+        </label>
+        <textarea id="content" rows="" cols="" required />
+        <button type="submit">등록하기</button>
+      </form>
+    </Card>
   );
 };
 
