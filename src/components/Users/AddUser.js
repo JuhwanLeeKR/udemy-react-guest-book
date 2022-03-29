2992283ba0328996ab8707be8dab2f65c7e9662e
Juhwan <dev.juhwanlee@gmail.com>
2022-03-29 23:19:57 +0900

    Add code: useState

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
@@ -6,17 +6,30 @@ import Button from '../UI/Button';
 import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState();
+  const [enteredPhone, setEnteredPhone] = useState();
+
   const addUserHandler = (event) => {
     event.preventDefault();
   };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+    console.log(enteredUsername, enteredPhone);
+  };
+
+  const phoneChangeHandler = (event) => {
+    setEnteredPhone(event.target.value);
+  };
+
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <div className={classes.inputFlex}>
           <label htmlFor="username">방문자명</label>
-          <input id="username" type="text" />
+          <input id="username" type="text" onChange={usernameChangeHandler} />
           <label htmlFor="phone">연락처</label>
-          <input id="phone" type="number" />
+          <input id="phone" type="number" onChange={phoneChangeHandler} />
         </div>
         <label htmlFor="content" className={classes.blind}>
           내용
