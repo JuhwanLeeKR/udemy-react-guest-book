79da893595620eb01bf3fbc68d2fd4dacebc4079
Juhwan <dev.juhwanlee@gmail.com>
2022-03-30 02:39:38 +0900

    Add code: use portal (ReactDOM)

--- src/App.js
+++ src/App.js
@@ -21,13 +21,14 @@ function App() {
   };
 
   return (
-    <React.Fragment>
+    <Fragment>
       <AddUser onAddUser={addUserHandler} />
       <UserList users={usersList} />
-    </React.Fragment>
+    </Fragment>
     // <> ... </> 을 사용하여도 무관하다.
     // 다만 유효한 HTML이 아니며, 모든 프로젝트에 적합합 JSX라고 할 수 없다.
     // 프로젝트 셋업이 이를 지원해야 사용 가능하다.
+    // Fragement를 import 해주면 React.Fragment가 아닌 Fragment로 사용 가능하다
   );
 }
 
